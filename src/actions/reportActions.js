e4a2c9d59a2caaaf117518e7fe8aefd93d7b4a3f
branmar97 <brandonmar97@gmail.com>
2022-04-16 10:51:56 -0400

    fix reports action dispatch

--- src/actions/reportActions.js
+++ src/actions/reportActions.js
@@ -1,9 +1,11 @@
 export const fetchReports = () => {
     return (dispatch) => {
-        dispatch({ type: 'ADD_REPORTS'})
         fetch('http://localhost:3000/api/v1/reports')
         .then(response => {
           return response.json()
+        })
+        .then(responseJSON => {
+          dispatch({ type: 'ADD_REPORTS', reports: responseJSON })
         })
     }
 } 
