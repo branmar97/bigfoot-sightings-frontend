ef36ace9b7b98922e18bdc495f6d6d51064f9faa
branmar97 <brandonmar97@gmail.com>
2022-04-16 10:51:37 -0400

    add reports action to report reducer

--- src/reducers/reportReducer.js
+++ src/reducers/reportReducer.js
@@ -1,5 +1,13 @@
 const manageReports = (state = { all: [], current: {} }, action) => {
-    
+    switch(action.type) {
+        case 'ADD_REPORTS':
+            return {
+                ...state,
+                all: action.reports,
+            }
+        default:
+            return state;
+    }
 }
 
 export default manageReports;
