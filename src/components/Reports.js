ae583fbbca4c9836fad99fc4b75954fe8e685c77
branmar97 <brandonmar97@gmail.com>
2022-04-17 12:19:04 -0400

    render all reports

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import ReportCard from './ReportCard';
 
-const Reports = () => {
+const Reports = ({ reports }) => {
+    const listReports = () => {
+        return reports.map(report => <ReportCard 
+            id={report.id}
+            caseNum={report.case_num}
+            occurence={report.occurence}
+            city={report.city}
+            state={report.state}
+            vicinity={report.vicinity}
+            conditions={report.conditions}
+            witnesses={report.witnesses}
+            evidence={report.evidence}
+            account={report.account}
+            prints={report.prints}
+            sounds={report.sounds}
+            additionalInfo={report.additional_info}
+            timeCreated={report.created_at}
+            lastUpdated={report.updated_at}
+        />)
+    }
 
     return ( 
         <div>
-            <h3>Report</h3>
+            {listLobbies()}
         </div>
      );
 }
